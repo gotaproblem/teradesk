b5532b89cfd0c8629b5b77729b5c47f18d642893
Miro Kropacek <miro.kropacek@gmail.com>
2026-07-14 10:51:40 +1000

    Replace last remaining tho-otto.de references with my server
    At least for now, ideally we set up proper GitHub Actions for those projects later.

--- .scripts/install-aranym.sh
+++ .scripts/install-aranym.sh
@@ -7,9 +7,9 @@ echo "ARANYM=$ARANYM" >> $GITHUB_ENV
 mkdir "$ARANYM"
 cd "$ARANYM"
 
-curl --get https://tho-otto.de/download/mag-hdd.tar.bz2 --output mag-hdd.tar.bz2
-#curl --get https://tho-otto.de/download/aranym-1.0.2-trusty-x86_64-a9de1ec.tar.xz --output aranym.tar.xz
-curl --get https://tho-otto.de/download/aranym-1.1.0-xenial-x86_64-4ebb186.tar.xz --output aranym.tar.xz
+curl -L --get https://mikro.atari.org/tho-otto.de/mag-hdd.tar.bz2 --output mag-hdd.tar.bz2
+#curl -L --get https://mikro.atari.org/tho-otto.de/aranym-1.0.2-trusty-x86_64-a9de1ec.tar.xz --output aranym.tar.xz
+curl -L --get https://mikro.atari.org/tho-otto.de/aranym-1.1.0-xenial-x86_64-4ebb186.tar.xz --output aranym.tar.xz
 
 tar xvf mag-hdd.tar.bz2
 test -f config-hdd || exit 1
